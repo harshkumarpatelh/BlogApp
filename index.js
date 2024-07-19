84b29afa9f473e15ea30ae3cfa646eb82700dab6
Harsh Patel <codeharsh13@gmail.com>
2024-07-20 00:13:05 +0530

    change-1

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
-
+const path = require("path");
 const app = express();
+
 const PORT = 8005;
 
+
+app.set("view engine", "ejs");
+app.set("views",path.resolve("./views"));
 app.listen(PORT, ()=>{
     console.log("Server Started at PORT: ", PORT);
 });
